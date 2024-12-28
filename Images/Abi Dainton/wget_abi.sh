5fbeffeb7f8b0c884a0a543fd12a25377ef352da
paranoidandroid-96 <dark_knight-96@hotmail.com>
2024-12-28 16:00:16 -0300

    Add images

---
+++ Images/Abi Dainton/wget_abi.sh
@@ -0,0 +1,2 @@
+wget https://diy-magazine.s3.amazonaws.com/d/diy/Artists/D/Daughter/Daughter-{0..24}-of-24-10072014-Abi-Dainton-2048x1365.jpg
+
---
+++ Images/Sarah Louise Bennett/wget_sarah.sh
@@ -0,0 +1,2 @@
+wget https://diy-magazine.s3.amazonaws.com/d/diy/Artists/D/Daughter/Latitude-2016/Daughter_Latitude-2016_SLB_-{0..200}.jpg
+
