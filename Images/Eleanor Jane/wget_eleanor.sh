2d1ec7bf77b65d51c456d4261bc5ddfb44900d72
paranoidandroid-96 <dark_knight-96@hotmail.com>
2025-03-15 21:06:52 -0300

    Add Emma Swann missing images, Eleanor Jane

---
+++ Images/Eleanor Jane/wget_eleanor.sh
@@ -0,0 +1,5 @@
+wget https://guitar.com/wp-content/uploads/2019/01/exre-interview-feature@1400x1050.jpg
+wget https://guitar.com/wp-content/uploads/2019/01/Elena-Tonra-interview-4ad-studios@1400x1050.jpg
+wget https://guitar.com/wp-content/uploads/2019/01/elena-tonra-interview-gibson@1400x1050.jpg
+wget https://guitar.com/wp-content/uploads/2019/01/elena-tonra-interview-telecaster@1400x1050.jpg
+wget https://guitar.com/wp-content/uploads/2019/01/elena-onra-interview-fender-amp@1400x1050.jpg
--- Images/Emma Swann/emma_exre.sh
+++ Images/Emma Swann/emma_exre.sh
@@ -1,3 +1,4 @@
+wget https://diy-magazine.s3.amazonaws.com/d/diy/Artists/E/Ex-re/CNV000{00..35}-web.jpg
 wget https://diy-magazine.s3.amazonaws.com/d/diy/Artists/E/Ex-re/159581-00{00..15}-web.jpg
 
 
