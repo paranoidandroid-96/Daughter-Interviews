0adb67dbe8b486b7f6212063c48e47015e555554
paranoidandroid-96 <dark_knight-96@hotmail.com>
2025-05-25 22:33:51 -0400

    Add DIY Magazine, Lados Issuu backup

---
+++ Backup/2013/2013-03-01/wget_diy.sh
@@ -0,0 +1,2 @@
+wget https://image.isu.pub/130228220226-109cb2202bb140358a41fcf87404dbd1/jpg/page_1.jpg
+wget https://image.isu.pub/130228220226-109cb2202bb140358a41fcf87404dbd1/jpg/page_{54..55}.jpg
---
+++ Backup/2015/2015-12-28/wget_lados.sh
@@ -0,0 +1,3 @@
+wget https://web.archive.org/web/20230302004658im_/https://image.isu.pub/151228154651-70218eaf7b0749f8626aea78659d7c54/jpg/page_1.jpg
+
+wget https://web.archive.org/web/20230302004658im_/https://image.isu.pub/151228154651-70218eaf7b0749f8626aea78659d7c54/jpg/page_{14..15}.jpg
