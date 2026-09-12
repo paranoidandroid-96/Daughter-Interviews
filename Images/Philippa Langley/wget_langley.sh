947153e8e9cb9b569b058f02daa46be69c2fd5f6
paranoidandroid-96 <dark_knight-96@hotmail.com>
2026-09-12 14:26:07 -0300

    Add Oh Comely interviews 2013, 2019, Philippa Langley photos

---
+++ Images/Philippa Langley/wget_langley.sh
@@ -0,0 +1,9 @@
+# https://cargocollective.com/philippalangley/Daughter
+
+wget https://payload.cargocollective.com/1/8/269030/5755576/DaugherHeroshot.jpg
+wget https://payload.cargocollective.com/1/8/269030/5755576/DaughterElena.jpg
+wget https://payload.cargocollective.com/1/8/269030/5755576/DaughterIgor.jpg
+wget https://payload.cargocollective.com/1/8/269030/5755576/DaughterRomi.jpg
+wget https://payload.cargocollective.com/1/8/269030/5755576/daughter-interview_905.jpg
+wget https://payload.cargocollective.com/1/8/269030/5755576/oh-comely-magazine.jpg
+
